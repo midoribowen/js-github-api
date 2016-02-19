3f495b235d50b0a0522b3412439200207e34188a
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-19 12:18:04 -0800

    Add jshint rule.

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('cssBuild', function() {
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'))
     .pipe(browserSync.stream());
 });
 

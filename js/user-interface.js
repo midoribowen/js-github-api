e0554804e0382835ba53c77fdb7589ab851a62d4
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-19 12:36:22 -0800

    Change user-interface #repos to divs. Still buggy.

--- js/user-interface.js
+++ js/user-interface.js
@@ -3,17 +3,17 @@ var User = require("./../js/user.js").User;
 $(function() {
   $("#repos").click(function() {
     var username = $("#username").val();
-    $.get('https://api.github.com/users/' + username + "/repos?", function(data) {
+    $("#username").val("");
+    $.get('https://api.github.com/users/' + username + "/repos", function(data) {
+      var user = new User(data);
       if (data.cod !== 200) {
         $("#show-repos").text(data.message);
       } else {
-        var repoName = (data.full_name);
-        var repoURL = (data.html_url);
         $("#show-repos").show();
         $("#user").text(username);
         for(var i = 0; i < data.length; i++) {
-          $("ul#repos").append("<li>Repository Name: " + data[i].repoName +
-                               "<li>URL: " + data[i].repoURL + "</li></li>")
+          $("div#repos").append("<ul><li>Repository Name: " + data[i].full_name +
+                               "<li>URL: " + data[i].html_url + "</li></li></ul>")
         }
       }
     });

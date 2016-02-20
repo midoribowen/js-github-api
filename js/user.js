79030d03455982c5695a59e41bc08536717d0169
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-19 22:22:27 -0800

    Add method to get user api data.

--- js/user.js
+++ js/user.js
@@ -1,12 +1,16 @@
 // Constructor
 var User = function(username) {
- this.username = username;
+  this.username = username;
 };
 
 // Methods
+User.prototype.userData = function() {
+  return 'https://api.github.com/' + this.username;
+}
 
-
-
+User.prototype.userReposData = function() {
+  return 'https://api.github.com/' + this.username + '/repos';
+};
 
 // Exports Class to *-interface.js
-exports.User = User;
+module.exports = User;

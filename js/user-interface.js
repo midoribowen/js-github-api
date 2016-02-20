0d07e73e7dc4a490fba7e61a2ba9458479d09421
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-19 22:21:40 -0800

    Have a bug where data queried is not displaying.

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,19 +1,30 @@
 var User = require("./../js/user.js").User;
 
 $(function() {
-  $("#repos").click(function() {
+  $("#username-form").submit(function(event) {
+    event.preventDefault();
+    var user = new User(username);
     var username = $("#username").val();
     $("#username").val("");
-    $.get('https://api.github.com/users/' + username + "/repos", function(data) {
-      var user = new User(data);
-      if (data.cod !== 200) {
-        $("#show-repos").text(data.message);
+    $.get(user.userData(), function(data) {
+      if (data !== 404) {
+        $("#show-user").text("Oopsy, daisy! Looks like this user doesn't exist!");
       } else {
-        $("#show-repos").show();
-        $("#user").text(username);
+        $("#show-user").show();
+        $("#user").text(data.login);
+        $("#followers").text(data.followers);
+        $("#public-repos").text(data.public_repos);
+      }
+    });
+    $.get(user.userReposData, function(data) {
+      if (data !== 404) {
+        ("#show-repos").text("Oopsy, daisy! This user may not have any public repositories!");
+      } else {
+        $("show-repos").show();
         for(var i = 0; i < data.length; i++) {
-          $("div#repos").append("<ul><li>Repository Name: " + data[i].full_name +
-                               "<li>URL: " + data[i].html_url + "</li></li></ul>")
+          $("div#show-repos ul").append("<li>Repository Name: " + "<a href='" + data[i].html_url + "'>" + data[i].name + "</a>" +
+                                        "<ul><li>Created At: " + moment(data[i].created_at).format('LLLL') + "</li>" +
+                                        "<li>" + data[i].description + "</li></ul></li>");
         }
       }
     });

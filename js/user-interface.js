d418721414e93df54c0d36ccf2ef5010b21ce446
Midori Bowen <midori.p.bowen@gmail.com>
2016-02-21 18:09:32 -0800

    Show list of repositories with name, last updated, created at, and description.

--- js/user-interface.js
+++ js/user-interface.js
@@ -13,16 +13,38 @@ $(function() {
       $(".show-user").show();
       $(".show-user").append("<img src='" + data.avatar_url + "' alt='user's avatar' height='200' width='200'>");
       if (data.name) {
-        $(".show-user").append("<h2 id='full-name'>" + data.name + "</h2>");
+        $(".show-user").append("<h2 id='full-name'>" +
+                               "<a href='" + data.html_url + "'>" +  data.name +
+                               "</a></h2>");
       }
       $(".show-user").append("<h3 id='user'>Username: " + data.login + "</h3><hr>" +
                              "<h4 id='followers'>" + data.followers + "<h6> Followers</h6></h4>" +
                              "<h4 id='number-repos'>" + data.public_repos + "<h6> Public Repositories</h6></h4>");
+    })
+    .fail(function() {
+      $(".show-user").append("<p align='center'>Oopsy, daisy! We couldn't find any users matching '" + username + "'</p>" +
+                             "<p align='center'>Try an " +
+                             "<a href='https://github.com/search/advanced'>advanced search" + "</a>!</p>");
     });
-    // $.get(user.userReposData(), function(data) {
-    //   if (data !== 404) {
-    //     ("#show-repos").text("Oopsy, daisy! This user may not have any public repositories!");
-    //   } else {
+
+    $.get(user.userReposData(), function(data) {
+      $(".public-repos").show();
+      for(var i = 0; i < data.length; i ++) {
+        $(".public-repos").append("<h3 id='repo-name'>" +
+                                  "<a href='" + data[i].html_url + "'>" + data[i].name + "</a>" +
+                                  "</h3>" +
+                                  "<h5 id='last-updated'>Last Updated: " +
+                                  moment(data[i].updated_at).calendar() +
+                                  "</h5>" +
+                                  "<h5 id='created-at'>Created: " + moment(data[i].created_at).format('LLLL') +
+                                  "</h5>");
+        if(data[i].description === "") {
+          $(".public-repos").append("<hr>")
+        } else {
+          $(".public-repos").append("<h4>Description:</h4>" + "<p align='left'>" + data[i].description + "</p>" + "<hr>");
+        }
+      }
+    });
     //     $("show-repos").show();
     //     for(var i = 0; i < data.length; i++) {
     //       $("div#show-repos ul").append("<li>Repository Name: " + "<a href='" + data[i].html_url + "'>" + data[i].name + "</a>" +
